c4c440ab23921646563afa9c0aca54386bd3ccdc
John Odom <joodom91@gmail.com>
2019-07-06 14:59:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,22 @@
-# require modules here
-
-def load_library
-  # code goes here
+ require "yaml"
+def load_library(yaml_file)
+  emoticons = YAML.load_file(yaml_file)
+  value = Hash.new
+  value["get_emoticon"]= Hash.new 
+  value["get_meaning"]= Hash.new
+      #value={"get_emoticon" => {}, "get_meaning"=>{}}
+  emoticons.map do |definition, faces|
+    american_emoticons = faces[0]
+    japanese_emoticons = faces[1]
+    value["get_meaning"][japanese_emoticons] = definition
+    value["get_emoticon"][american_emoticons] = japanese_emoticons
+  end
+  return value
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yaml_file, emoticons)
+  load_library(yaml_file)
+  
 end
 
 def get_english_meaning
